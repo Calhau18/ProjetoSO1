ed954acd53f8bc363685020d60956a84ac909302
Martim Moniz <martimrmoniz@gmail.com>
2022-01-22 21:04:25 +0000

    Update destroy_after_all to be multi-threaded

--- tests/lib_destroy_after_all_closed_test.c
+++ tests/lib_destroy_after_all_closed_test.c
@@ -14,38 +14,52 @@
     operation).
 */
 
-int closed_file = 0;
-int f;
+char* path = "/a";
+char* msg = "A";
+int counter = 0;
 
 void *fn_thread(void *arg) {
     (void)
         arg; /* Since arg is not used, this line prevents a compiler warning */
 
-    sleep(10);
+    ssize_t r;
+    int f;
 
-    /* set *before* closing the file, so that it is set before
-       tfs_destroy_after_all_close returns in the main thread
-    */
-    closed_file = 1;
+    while (1) {
+        f = tfs_open(path, TFS_O_CREAT);
+        if (f == -1) {
+            break;
+        } // No more files can be opened, system is being destroyed
 
-    assert(tfs_close(f) != -1);
+        r = tfs_write(f, msg, 1);
+        assert(r == (ssize_t)1);
+        counter++;
+
+        assert(tfs_close(f) != -1);
+    }
 
     return NULL;
 }
 
-int main() {
-
+int main(int argc, char** argv) {
+    int count = 0;
     assert(tfs_init() != -1);
 
-    pthread_t t;
-    f = tfs_open("/f1", TFS_O_CREAT);
-    assert(f != -1);
+    if (argc > 1)
+        count = atoi(argv[1]);
 
-    assert(pthread_create(&t, NULL, fn_thread, NULL) == 0);
+    if (count <= 0)
+        count = 1;
+
+    pthread_t tid[count];
+
+    for (int i=0; i<count; i++) {
+        assert(pthread_create(&tid[i], NULL, fn_thread, NULL) == 0);
+    }
     assert(tfs_destroy_after_all_closed() != -1);
-    assert(closed_file == 1);
 
-    // No need to join thread
+    // No need to join threads, they've all finished by now
+    printf("Wrote %d times.\n", counter);
     printf("Successful test.\n");
 
     return 0;
